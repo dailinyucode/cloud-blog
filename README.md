36fa9d7d18520f11eb4baa97ae9249ab5a2fd74e
Daily <>
2023-10-18 08:40:20 +0800

    feat(init): 初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+### 从0开始搭建一套Cloud的博客项目
+
+后端技术站：springboot3 + jdk17
+项目构建：gradle
---
+++ blog-gateway/src/main/java/com/dly/blog/gateway/BlogGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.dly.blog.gateway;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class ,args);
+    }
+}
